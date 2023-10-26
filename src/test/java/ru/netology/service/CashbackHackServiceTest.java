92af0ce74dfc2a8febd2c8386a3b31c8d1b1cea4
Demidoff Serg <serega.dem@inbox.ru>
2023-10-26 20:08:20 +0800

    add changes

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,50 @@
+package ru.netology.service;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(expected,actual);
+    }
+
+    @org.junit.Test
+    public void testRemainWithCashback() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(expected,actual);
+    }
+
+    @org.junit.Test
+    public void testRemain1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(expected,actual);
+    }
+
+    @org.junit.Test
+    public void testRemainWithCashback1() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        assertEquals(expected,actual);
+    }
+
+    @org.junit.Test
+    public void testRemain2() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 999;
+        assertEquals(expected,actual);
+    }
+}
